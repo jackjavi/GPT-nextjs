5b6439900fcd351223952e22c61e23517d11dc50
jack javi <jackmtembete@gmail.com>
2023-06-11 20:24:41 +0300

    Modify Home component for academic descriptions and vibe selection.

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,26 +16,30 @@ import {
 
 const Home: NextPage = () => {
   const [loading, setLoading] = useState(false);
-  const [description, setDescription] = useState("");
+  const [bio, setBio] = useState("");
   const [vibe, setVibe] = useState<VibeType>("Professional");
-  const [generatedDescriptions, setGeneratedDescriptions] =
-    useState<string>("");
+  const [generatedBios, setGeneratedBios] = useState<String>("");
 
-  const descriptionRef = useRef<HTMLDivElement | null>(null);
+  const bioRef = useRef<null | HTMLDivElement>(null);
 
-  const scrollToDescriptions = () => {
-    if (descriptionRef.current !== null) {
-      descriptionRef.current.scrollIntoView({ behavior: "smooth" });
+  const scrollToBios = () => {
+    if (bioRef.current !== null) {
+      bioRef.current.scrollIntoView({ behavior: "smooth" });
     }
   };
 
-  const prompt = `Generate 2 ${vibe} academic descriptions. Make sure each generated description is concise and accurately represents your research interests, academic background, and goals. Provide a short summary of your work in the context of ${description}${
-    description.slice(-1) === "." ? "" : "."
+  const prompt = `Generate 2 ${vibe} twitter biographies with no hashtags and clearly labeled "1." and "2.". ${
+    vibe === "Funny"
+      ? "Make sure there is a joke in there and it's a little ridiculous."
+      : null
+  }
+      Make sure each generated biography is less than 160 characters, has short sentences that are found in Twitter bios, and base them on this context: ${bio}${
+    bio.slice(-1) === "." ? "" : "."
   }`;
 
-  const generateDescriptions = async (e: React.FormEvent) => {
+  const generateBio = async (e: any) => {
     e.preventDefault();
-    setGeneratedDescriptions("");
+    setGeneratedBios("");
     setLoading(true);
     const response = await fetch("/api/generate", {
       method: "POST",
@@ -61,13 +65,13 @@ const Home: NextPage = () => {
       if (event.type === "event") {
         const data = event.data;
         try {
-          const text = JSON.parse(data).text ?? "";
-          setGeneratedDescriptions((prev) => prev + text);
+          const text = JSON.parse(data).text ?? ""
+          setGeneratedBios((prev) => prev + text);
         } catch (e) {
           console.error(e);
         }
       }
-    };
+    }
 
     // https://web.dev/streams/#the-getreader-and-read-methods
     const reader = data.getReader();
@@ -80,14 +84,14 @@ const Home: NextPage = () => {
       const chunkValue = decoder.decode(value);
       parser.feed(chunkValue);
     }
-    scrollToDescriptions();
+    scrollToBios();
     setLoading(false);
   };
 
   return (
     <div className="flex max-w-5xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
       <Head>
-        <title>Academic Description Generator</title>
+        <title>Twitter Bio Generator</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -103,11 +107,9 @@ const Home: NextPage = () => {
           <p>Star on GitHub</p>
         </a>
         <h1 className="sm:text-6xl text-4xl max-w-[708px] font-bold text-slate-900">
-          Generate academic descriptions using chatGPT
+          Generate your next Twitter bio using chatGPT
         </h1>
-        <p className="text-slate-500 mt-5">
-          47,118 descriptions generated so far.
-        </p>
+        <p className="text-slate-500 mt-5">47,118 bios generated so far.</p>
         <div className="max-w-xl w-full">
           <div className="flex mt-10 items-center space-x-3">
             <Image
@@ -118,25 +120,25 @@ const Home: NextPage = () => {
               className="mb-5 sm:mb-0"
             />
             <p className="text-left font-medium">
-              Copy your current description{" "}
+              Copy your current bio{" "}
               <span className="text-slate-500">
-                (or write a few sentences about your academic background)
+                (or write a few sentences about yourself)
               </span>
               .
             </p>
           </div>
           <textarea
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
+            value={bio}
+            onChange={(e) => setBio(e.target.value)}
             rows={4}
             className="w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black my-5"
             placeholder={
-              "e.g. PhD candidate in Computer Science specializing in Machine Learning and Natural Language Processing."
+              "e.g. Senior Developer Advocate @vercel. Tweeting about web development, AI, and React / Next.js. Writing nutlope.substack.com."
             }
           />
           <div className="flex mb-5 items-center space-x-3">
             <Image src="/2-black.png" width={30} height={30} alt="1 icon" />
-            <p className="text-left font-medium">Select the academic vibe.</p>
+            <p className="text-left font-medium">Select your vibe.</p>
           </div>
           <div className="block">
             <DropDown vibe={vibe} setVibe={(newVibe) => setVibe(newVibe)} />
@@ -145,9 +147,9 @@ const Home: NextPage = () => {
           {!loading && (
             <button
               className="bg-black rounded-xl text-white font-medium px-4 py-2 sm:mt-10 mt-8 hover:bg-black/80 w-full"
-              onClick={generateDescriptions}
+              onClick={(e) => generateBio(e)}
             >
-              Generate academic descriptions &rarr;
+              Generate your bio &rarr;
             </button>
           )}
           {loading && (
@@ -166,33 +168,33 @@ const Home: NextPage = () => {
         />
         <hr className="h-px bg-gray-700 border-1 dark:bg-gray-700" />
         <div className="space-y-10 my-10">
-          {generatedDescriptions && (
+          {generatedBios && (
             <>
               <div>
                 <h2
                   className="sm:text-4xl text-3xl font-bold text-slate-900 mx-auto"
-                  ref={descriptionRef}
+                  ref={bioRef}
                 >
-                  Generated academic descriptions
+                  Your generated bios
                 </h2>
               </div>
               <div className="space-y-8 flex flex-col items-center justify-center max-w-xl mx-auto">
-                {generatedDescriptions
-                  .substring(generatedDescriptions.indexOf("1") + 3)
+                {generatedBios
+                  .substring(generatedBios.indexOf("1") + 3)
                   .split("2.")
-                  .map((generatedDescription) => {
+                  .map((generatedBio) => {
                     return (
                       <div
                         className="bg-white rounded-xl shadow-md p-4 hover:bg-gray-100 transition cursor-copy border"
                         onClick={() => {
-                          navigator.clipboard.writeText(generatedDescription);
-                          toast("Description copied to clipboard", {
+                          navigator.clipboard.writeText(generatedBio);
+                          toast("Bio copied to clipboard", {
                             icon: "✂️",
                           });
                         }}
-                        key={generatedDescription}
+                        key={generatedBio}
                       >
-                        <p>{generatedDescription}</p>
+                        <p>{generatedBio}</p>
                       </div>
                     );
                   })}
